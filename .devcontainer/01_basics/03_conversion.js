04541a3ea712bb6cb8d683a38d70b434f00ac06a
Sharanyaa G <sharanyaag30@gmail.com>
2026-02-26 13:54:44 +0000

    coversion

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+let score = "10abc0"
+
+
+// const {score} = req.body
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "abc33" => NaN
+// "10abc0" => NaN
+// true => 1; false => 0
+// null => 0; undefined => NaN
+
+
+let isloggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+// "sharanyaa" => true
+// null => false; undefined => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
