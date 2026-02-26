eee4de42dcacd829fed51112c94bb432fa973d75
Sharanyaa G <sharanyaag30@gmail.com>
2026-02-26 14:03:19 +0000

    moved to seperate ile

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -3,13 +3,13 @@ let score = "10abc0"
 
 // const {score} = req.body
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -22,7 +22,7 @@ console.log(valueInNumber);
 let isloggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // ""=> false
@@ -31,5 +31,15 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+// *******************operations***********************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
