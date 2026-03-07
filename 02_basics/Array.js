f4af3956f1babcb4d0bdb8b8c462d6e42af66fb0
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-07 13:28:18 +0000

    Array1

---
+++ 02_basics/Array.js
@@ -0,0 +1,62 @@
+// array
+
+const myARr = [1, 2, 3, 4, 5];
+const myARr2 = ['hello', 'world', 'javascript'];
+const myARr3 = new Array(1, 2, 3, 4, 5);
+
+// console.log(myARr);
+// console.log(myARr.length);
+// console.log(myARr[0]);
+// console.log(myARr[myARr.length - 1]);
+// console.log(myARr2);
+// console.log(myARr2.length);
+// console.log(myARr2[0]);
+// console.log(myARr2[myARr2.length - 1]);
+// console.log(myARr3);
+// console.log(myARr3.length);
+// console.log(myARr3[0]);
+// console.log(myARr3[myARr3.length - 1]);
+
+// Array methods
+
+// myARr.push(6);
+// console.log(myARr);
+
+// myARr.pop();
+// console.log(myARr);
+
+// myARr.unshift(0);
+// console.log(myARr);
+
+// myARr.shift();
+// console.log(myARr);
+
+// myARr.splice(1, 2,'a', 'b', 'c');
+// console.log(myARr);
+
+// console.log(myARr.slice(1, 4));
+
+// console.log(myARr.includes(3));
+// console.log(myARr.indexOf(8));
+
+// const newarr = myARr.join();
+// console.log(myARr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+console.log("A" , myARr);
+
+const  myn1 =myARr.slice(1,3)
+console.log(myn1);
+
+console.log("B" , myARr);
+
+const  myn2 =myARr.splice(1,3)
+console.log("C", myARr);
+
+console.log(myn2);
+
+
+
+
+
