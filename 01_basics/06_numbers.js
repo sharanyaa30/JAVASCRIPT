1efc432752fb5c36d9c47420ab4f5f2bc8f02b05
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-07 12:11:29 +0000

    Math

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,46 @@
+// const score = 400;
+
+// const balance = new Number(100);
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(score.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherBalance = 123.456789;
+
+// console.log(otherBalance.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ======================== Maths =========================
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(66.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.7));
+console.log(Math.min(3, 5, 1, 8));
+console.log(Math.max(3, 5, 1, 8));
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10));
+
+
+const Min = 10;
+const Max = 20;
+
+console.log(Math.floor(Math.random() * (Max - Min + 1)) + Min);
+
+
+
+// math. random give values from 0 to 1 but not including 1. so we need to multiply it with the range of numbers we want and then add the minimum value to it. This way we can get a random number between the minimum and maximum values.
+
+
+
+
