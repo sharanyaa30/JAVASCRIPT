7ea70dff32874fb6eb6ee22f8587bbc7f92bb972
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-11 16:45:52 +0000

    function objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,3 +38,43 @@ function loginUserMessage(username = "Guest"){ // default parameter value
 console.log(loginUserMessage()); // function call with argument
 
 // if values are not passed it will be undefined
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+// first item will considered as num1 and second item will be ignored because we have only one parameter in the function
+
+//... rest operator will take all the values and store in an array and return the array
+
+// if the values are named parameters then we can use object destructuring to get the values from the object the rest will be in spread operator
+
+// const user = {
+//     username: "Sharanyaa",
+//     price: 500
+// }
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+handleObject({
+    username: "Sharanyaa",
+    price: 500
+}
+);
+
+const myNewArray = [1, 2, 3, 4, 5];
+
+function returnsecodvalue(getarray){
+    return myNewArray[1];
+}
+
+// console.log(returnsecodvalue(myNewArray));
+
+console.log(returnsecodvalue([1, 2, 3, 4, 5]));
+
