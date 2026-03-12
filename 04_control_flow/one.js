5634e4b275e5b2d36505d172a29b206b525fb10f
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-12 17:24:47 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,69 @@
+// if
+const isuserloggedin = true
+
+const temperature = 91
+
+// if (temperature === 51){
+//     console.log("less than 50");
+    
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("executed");
+
+
+// const score = 200
+
+// if (score > 100){
+//     var power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+
+// console.log(`user power: ${power}`);
+
+//var is global scope
+// for let and const it wont work 
+
+
+//>,<,<=,>=,==,!=, ===
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");
+
+// if (balance < 500){
+//     console.log("less than 500");
+    
+// }else if (balance < 750){
+//     console.log("less than 750");
+    
+// }else if (balance < 900){
+//     console.log("less than 900");
+    
+// }else{
+//     console.log("invalid");
+    
+// }
+
+
+const userloggedin = true 
+const loggedinfromgoogle = false
+const debitcard = true 
+const loggedinfromemail = true
+
+if (userloggedin && debitcard && 2 == 3 ){
+    console.log("allow");
+    
+}
+
+if (loggedinfromgoogle || loggedinfromemail || true){
+    console.log("logged in");
+    
+}
+
+
+
+
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    default:
+        console.log("default");
+        break;
+        
+        
+}
