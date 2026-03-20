b023d2673ac2b35b50e1685e5839835f6c5a5e52
Sharanyaa G <sharanyaag30@gmail.com>
2026-03-20 13:27:11 +0000

    Object enumerable dercripter

---
+++ 10_classes_and_oop/Mathpi.js
@@ -0,0 +1,41 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+//tell about ojects hidden phase
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+//console.log(descripter);
+
+
+const chai = {
+    name : 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nahi bani");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"))
+
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"))
+
+
+
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+    
+    
+}
